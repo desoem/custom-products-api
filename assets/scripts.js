8c21a6470c9f9ddc64366f255f441e6a6ef43ab4
Ridwan Sumantri <49280370+desoem@users.noreply.github.com>
2024-11-24 14:13:07 +0800

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,31 @@
-jQuery(document).ready(function ($) {
-    // Example script: Add more interactivity if needed
-    console.log('Custom Products API Plugin is loaded.');
+function fetchProducts(page = 1, search = '') {
+    jQuery.ajax({
+        url: ajaxurl,
+        method: 'POST',
+        data: {
+            action: 'fetch_products',
+            page: page,
+            search: search
+        },
+        success: function (response) {
+            jQuery('#product-list').html(response);
+        },
+        error: function () {
+            jQuery('#product-list').html('<p>Error fetching products.</p>');
+        }
+    });
+}
+
+jQuery(document).ready(function () {
+    fetchProducts();
+
+    jQuery(document).on('click', '.pagination a', function (e) {
+        e.preventDefault();
+        const page = jQuery(this).data('page');
+        fetchProducts(page, jQuery('#product-search').val());
+    });
+
+    jQuery('#product-search').on('input', function () {
+        fetchProducts(1, jQuery(this).val());
+    });
 });
